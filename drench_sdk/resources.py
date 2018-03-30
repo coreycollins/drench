35427c097a300278cc50fd16d68f3e402cd84056
Edward Jaros <edjaros@gmail.com>
2018-03-30 08:59:19 -0500

    add resource_type to arns

--- drench_sdk/resources.py
+++ drench_sdk/resources.py
@@ -5,18 +5,17 @@ class Resources(object): # pylint: disable=too-few-public-methods
     """ This class build a aws arn around specified resource types """
 
     @classmethod
-    def get_arn(cls, service, resource, region='us-east-1', partition='aws'):
+    def get_arn(cls, service, resource_type, resource, region='us-east-1', partition='aws'):
         """ get an arn from aws resource params """
         account_id = os.getenv('AWS_ACCOUNT_ID', '*')
-        env=os.getenv('DRENCH_ENV', '*')
 
         # IAM, Route53 dont require a region
         if service in ['iam', 'route53', 'artifact', 'cloudfront', 'organizations', 's3', 'waf']:
             region = ''
 
-        subtype = ''
-        if service == 'lambda':
-            subtype = 'function'
-
-
-        return f"arn:{partition}:{service}:{region}:{account_id}:{subtype}:{env}-{resource}"
+        #TODO elif this: not all services have resourcetypes, and some specify
+        #   resourcetype/resource
+        #        rather than
+        #   resourcetype:resource
+        # see https://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html
+        return f"arn:{partition}:{service}:{region}:{account_id}:{resource_type}:{resource}"
