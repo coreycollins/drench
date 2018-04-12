a6da5a1e804bd299daa912394fc0032000839d2b
Edward Jaros <edjaros@gmail.com>
2018-04-11 21:04:14 -0500

    call lambda directly in add_result

--- lambda/add_result.py
+++ lambda/add_result.py
@@ -1,24 +1,26 @@
 '''update drench-api with stepp function step results'''
-import requests
-
+import json
+import boto3
 
 def handler(event, context): # pylint:disable=unused-argument
     '''lambda interface'''
 
-    if 'payload' in event:
-        payload = event['payload']
-    else:
-        raise BaseException("No result payload sent")
+    payload = {
+        'body': json.dumps(event['result']),
+        'requestContext': {
+            'authorizer': {
+                'principalId': 1234 #FIXME hard code?
+                }
+            },
+        'queryStringParameters': {},
+        'headers': {},
+        'httpMethod': 'PUT',
+        'path': '/jobs/{event["job_id"]}/steps'
+    }
 
-    headers = {'x-drench-token': 'test_token'}
+    client = boto3.client('lambda')
 
-    job_id = payload['step']['job_id']
-    resp = requests.put(f'https://api.drench.io/jobs/{job_id}/steps',
-                        data=payload,
-                        headers=headers
-                       )
-
-    if resp.status_code == requests.codes['ok']:
-        return 'SUCCEEDED'
-
-    return 'FAILED'
+    client.invoke(
+        FunctionName='arsa-drench-api:v1',
+        Payload=json.dumps(payload).encode()
+    )
