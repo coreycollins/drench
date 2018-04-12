dbe09cb4f8db20740ab2e3e66a7ea11b91f9da41
Edward Jaros <edjaros@gmail.com>
2018-04-12 13:08:14 -0500

    add test coverage for add_result lambda

---
+++ tests/test_add_result.py
@@ -0,0 +1,35 @@
+#pylint:disable=missing-docstring
+from lambdas.add_result import handler
+
+
+def test_add_result():
+    event = {
+        'job_id': 1234,
+        'account_id': 4321,
+        'next': {
+            'in_path': 'some/path',
+            'out_path': 's3://com.drench.results/1234/test-query/out',
+            'content_type': 'text',
+            'report_url': None,
+            'name': 'test-query',
+            'type': 'query',
+            'params': {
+                'QueryString': 'SELECT *',
+                'ResultConfiguration': {
+                    'OutputLocation': '$.next.out_path'
+                },
+                "QueryExecutionContext": {
+                    "Database": "foo"
+                },
+            },
+        },
+        'result': {
+            'name': 'test-query',
+            'out_path': 's3://com.drench.results/1234/test-query/out',
+            'content_type': 'text',
+            'report_url': None,
+            'status': 'pass'
+        }
+    }
+
+    handler(event, {})
