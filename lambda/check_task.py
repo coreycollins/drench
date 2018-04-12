82065268c3b041dbb4d843e90d6b2acd4835219a
Edward Jaros <edjaros@gmail.com>
2018-04-11 20:36:56 -0500

    populate $.result in check_task lambda

--- lambda/check_task.py
+++ lambda/check_task.py
@@ -4,7 +4,14 @@ import boto3
 def handler(event, context): #pylint:disable=unused-argument
     '''default lambda interface'''
 
+    #FIXME case-switch support different resources (batch, athena, etc)
     client = boto3.client('glue', region_name='us-east-1')
     response = client.get_job_run(JobName=event['next']['params']['JobName'], RunId=event['result']['runId'])
+    status = response['JobRun']['JobRunState']
 
-    return response['JobRun']['JobRunState']
+    return {
+        'name': event['next']['name'],
+        'out_path': event['next']['out_path'],
+        'content_type': event['next']['content_type'],
+        'status': status
+    }
