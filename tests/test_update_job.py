f8bddc4f853582095b750069a82aaae1f302cd03
Edward Jaros <edjaros@gmail.com>
2018-04-20 14:57:10 -0500

    remove duplicate code in update test

--- tests/test_update_job.py
+++ tests/test_update_job.py
@@ -24,37 +24,9 @@ def test_update_job_happy_case():
             },
         },
         'result': {
-            'name': 'test-query',
             'out_path': 's3://com.drench.results/1234/test-query/out',
-            'content_type': 'text',
-            'report_url': None,
             'status': 'pass'
         }
-    }
-
-    handler(event, {})
-
-def test_update_job_happy_case():
-    event = {
-        'api_version': 'v0',
-        'job_id': 1234,
-        'principal_id': 4321,
-        'next': {
-            'in_path': 'some/path',
-            'out_path': 's3://com.drench.results/1234/test-query/out',
-            'content_type': 'text',
-            'name': 'test-query',
-            'type': 'query',
-            'params': {
-                'QueryString': 'SELECT *',
-                'ResultConfiguration': {
-                    'OutputLocation': '$.next.out_path'
-                },
-                "QueryExecutionContext": {
-                    "Database": "foo"
-                },
-            },
-        },
     }
 
     handler(event, {})
