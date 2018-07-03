65b179ac39f0b557aa5096432afe73c951bcb81b
Corey Collins <coreyecollins@gmail.com>
2018-07-03 09:26:17 -0500

    fix sns test

--- tests/test_send_sns.py
+++ tests/test_send_sns.py
@@ -5,7 +5,7 @@ from lambdas.send_sns import handler
 def test_add_result():
     event = {
         'topic_arn': 'some:long:arn',
-        'job_id': 1234
+        'result':{'status':'finished'}
     }
 
     handler(event, {})
