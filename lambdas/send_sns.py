2e240fe96359d49ffc2b2f1993e2d0706eaaac15
Corey Collins <coreyecollins@gmail.com>
2018-07-03 14:14:29 -0500

    remove api dependecy

--- lambdas/send_sns.py
+++ lambdas/send_sns.py
@@ -1,22 +1,25 @@
 '''send status message through sns with channel config'''
+import json
 import boto3
+
+SUBJECTS = {
+    'running': 'The job has started running',
+    'finished': 'The job has finished',
+    'failed': 'The job has failed'
+}
 
 def handler(event, context): #pylint:disable=unused-argument
     '''default lambda interface'''
     client = boto3.client('sns', region_name='us-east-1')
 
     topic_arn = event['topic_arn']
-
-    #FIXME This is a direct dependcy from the API and needs to be fixed
-    job_id = event['job_id']
-    account_id = event['account_id']
+    status = event['result']['status']
 
     client.publish(
         TopicArn=topic_arn,
-        Message=event['result']['status'],
+        Subject=SUBJECTS[status],
+        Message=json.dumps(event, indent=4),
         MessageAttributes={
-            'message_type': {'DataType':'String', 'StringValue':'sfn'},
-            'account_id': {'DataType':'String', 'StringValue': str(account_id)},
-            'job_id': {'DataType':'String', 'StringValue': str(job_id)}
+            'message_type': {'DataType':'String', 'StringValue':'sfn'}
         }
     )
