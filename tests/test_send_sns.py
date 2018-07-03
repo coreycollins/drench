2c5d94fcc6f1f13916671d3756ba9d45bfa22186
Corey Collins <coreyecollins@gmail.com>
2018-07-03 13:27:05 -0500

    fixed tests

--- tests/test_send_sns.py
+++ tests/test_send_sns.py
@@ -5,6 +5,8 @@ from lambdas.send_sns import handler
 def test_add_result():
     event = {
         'topic_arn': 'some:long:arn',
+        'job_id': 123,
+        'account_id': 123,
         'result':{'status':'finished'}
     }
 
