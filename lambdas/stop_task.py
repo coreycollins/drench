57e1e6e2f74358577fcc71f5017edcfc693dc5b7
Ed Jaros <ejaros@cmsdm.com>
2018-08-06 16:02:11 -0500

    add reason for cancelled batch kobs; correct lookups for details, jobname; prefer get with default for getting function from runner dict; use correct statefunctions aws service name

--- lambdas/stop_task.py
+++ lambdas/stop_task.py
@@ -7,33 +7,33 @@ def handler(event, _):
     def stop_glue(task_output):
         '''cancel glue job'''
         client = boto3.client('glue', region_name='us-east-1')
-        res = client.batch_stop_job_run(JobName=task_output['next']['JobName'],
+        res = client.batch_stop_job_run(JobName=task_output['next']['params']['JobName'],
                                         JobRunIds=[task_output['result']['job_id']])
 
-        fail_job = next(j for j in res['Errors'] if j['JobRunId'] == task_output['result']['job_id'])
-        if fail_job:
+        try:
+            fail_job = next(j for j in res['Errors'] if j['JobRunId'] == task_output['result']['job_id'])
             raise Exception(fail_job['ErrorDetail']['ErrorMessage'])
+        except StopIteration:
+            pass
 
     def stop_batch(task_output):
         '''cancel batch job'''
         client = boto3.client('batch', region_name='us-east-1')
-        client.terminate_job(jobId=task_output['result']['job_id'])
+        client.terminate_job(jobId=task_output['result']['job_id'], reason='user cancelled')
 
     runner = {
         'glue': stop_glue,
         'batch': stop_batch,
     }
 
-    res = boto3.client('sfn', region_name='us-east-1').get_execution_history(
+    res = boto3.client('stepfunctions', region_name='us-east-1').get_execution_history(
         executionArn=event['detail']['requestParameters']['executionArn'],
         reverseOrder=True)
 
     try:
-        task_output = next(e['output'] for e in res['events'] if e['type'] == 'TaskStateExited')
-        try:
-            runner[task_output['next']['type']](task_output)
-            return None
-        except KeyError:
-            return None
+        task_output = next(e['stateExitedEventDetails']['output'] for e in res['events'] if e['type'] == 'TaskStateExited')
     except StopIteration:
-        return None
+        return "OK"
+
+    runner.get(task_output['next']['type'], lambda: None)(task_output)
+    return "OK"
